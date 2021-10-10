ed4f546d41145a85ba5aa3314332ac14f595d2fe
FedericoTartarini <federicotartarini@gmail.com>
2021-10-10 21:20:27 +0800

    copied code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import useSwr from "swr";
+import useSupercluster from "use-supercluster";
+import "./App.css";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+const cuffs = new L.Icon({
+  iconUrl: "/handcuffs.svg",
+  iconSize: [25, 25]
+});
+
+const icons = {};
+const fetchIcon = (count, size) => {
+  if (!icons[count]) {
+    icons[count] = L.divIcon({
+      html: `<div class="cluster-marker" style="width: ${size}px; height: ${size}px;">
+        ${count}
+      </div>`
+    });
+  }
+  return icons[count];
+};
+
+export default function App() {
+  const [bounds, setBounds] = useState(null);
+  const [zoom, setZoom] = useState(13);
+  const mapRef = useRef();
+
+  function updateMap() {
+    const b = mapRef.current.leafletElement.getBounds();
+    setBounds([
+      b.getSouthWest().lng,
+      b.getSouthWest().lat,
+      b.getNorthEast().lng,
+      b.getNorthEast().lat
+    ]);
+    setZoom(mapRef.current.leafletElement.getZoom());
+  }
+
+  React.useEffect(() => {
+    updateMap();
+  }, []);
+
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, fetcher);
+  const crimes = data && !error ? data : [];
+  const points = crimes.map(crime => ({
+    type: "Feature",
+    properties: { cluster: false, crimeId: crime.id, category: crime.category },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(crime.location.longitude),
+        parseFloat(crime.location.latitude)
+      ]
+    }
+  }));
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 75, maxZoom: 17 }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      center={[52.6376, -1.135171]}
+      zoom={13}
+      onMoveEnd={updateMap}
+      ref={mapRef}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {clusters.map(cluster => {
+        // every cluster point has coordinates
+        const [longitude, latitude] = cluster.geometry.coordinates;
+        // the point may be either a cluster or a crime point
+        const {
+          cluster: isCluster,
+          point_count: pointCount
+        } = cluster.properties;
+
+        // we have a cluster to render
+        if (isCluster) {
+          return (
+            <Marker
+              key={`cluster-${cluster.id}`}
+              position={[latitude, longitude]}
+              icon={fetchIcon(
+                pointCount,
+                10 + (pointCount / points.length) * 40
+              )}
+              onClick={() => {
+                const expansionZoom = Math.min(
+                  supercluster.getClusterExpansionZoom(cluster.id),
+                  17
+                );
+                const leaflet = mapRef.current.leafletElement;
+                leaflet.setView([latitude, longitude], expansionZoom, {
+                  animate: true
+                });
+              }}
+            />
+          );
+        }
+
+        // we have a single point (crime) to render
+        return (
+          <Marker
+            key={`crime-${cluster.properties.crimeId}`}
+            position={[latitude, longitude]}
+            icon={cuffs}
+          />
+        );
+      })}
+    </Map>
   );
 }
-
-export default App;
